410a3151af0532e5e4ba00565b98559f8fedabc1
yonezawaketa <e195731@ie.u-ryukyu.ac.jp>
2021-01-21 12:08:33 +0900

    共有のためにコミット

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,27 @@
 <template>
     <v-main>
+      <v-card width="70vw">
+        <v-expansion-panels>
+          <v-expansion-panel>
+            <v-expansion-panel-header hide-actions>
+              <v-toolbar
+                v-on:click="mouseClickopen"
+                flat
+                color="blue-grey"
+                dark
+              ><v-toolbar-title>タスクを入力＋</v-toolbar-title>
+              </v-toolbar>
+            </v-expansion-panel-header>
+            <v-card-text>
+              <v-expansion-panel-content>
       <v-row dense>
         <v-col>
-          <v-text-field v-on:click="mouseClickHandler" label="タスクを入力" v-model="inputTitle">
+          <v-text-field v-if="isClicked" label="タスクを入力" v-model="inputTitle">
           </v-text-field>
+
           <v-text-field v-if="isClicked" label="タスク詳細" v-model="inputDetail">
           </v-text-field>
+
           <v-select　v-if="isClicked"
             label="科目を選択"
             :items="subject"
@@ -13,8 +29,17 @@
             item-value="id"
             v-model="inputId"
           ></v-select>
-          <v-btn v-if="isClicked" v-on:click="addTasks">登録</v-btn>
+
+          <v-btn v-if="isClicked" v-on:click="addTasks();mouseClickclose()">登録</v-btn>
+
         </v-col>
+        </v-row>
+        </v-expansion-panel-content>
+
+      </v-card-text>
+      </v-expansion-panel>
+      </v-expansion-panels>
+        </v-card>
         <v-col
           v-for="(item, i) in tasks"
           :key="i"
@@ -22,7 +47,6 @@
         >
           <TaskCard :color="getSubjectData(item.subjectId).color" :title="item.title" :description="item.desc" :id="item.id" :subjectName="getSubjectData(item.subjectId).name" :subjectYoubi="getSubjectData(item.subjectId).youbi"></TaskCard>
         </v-col>
-      </v-row>
     </v-main>
 </template>
 
@@ -40,8 +64,11 @@ export default {
     inputId: '',
   }),
   methods: {
-    mouseClickHandler(){
-      this.isClicked = true
+    mouseClickopen(){
+      this.isClicked = true;
+    },
+    mouseClickclose(){
+      this.isClicked = false;
     },
     addTasks() {
       this.$store.dispatch('addTask', {
